b1d412118bf96bcfe6a44edda2b5d4ffec213e0a
Roberto Jr <rrjr87@outlook.com>
2021-08-13 17:30:37 -0300

    refactoring

--- src/main/java/Beginner/p1010.java
+++ src/main/java/Beginner/p1010.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 public class p1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int codigo1 = sc.nextInt();
-        int quantidade1 = sc.nextInt();
+        int cod1 = sc.nextInt(), quantidade1 = sc.nextInt();
         double preco1 = sc.nextDouble();
-        int codigo2 = sc.nextInt();
-        int quantidade2 = sc.nextInt();
+        int cod2 = sc.nextInt(), quantidade2 = sc.nextInt();;
         double preco2 = sc.nextDouble();
         double valorAPagar = (preco1 * quantidade1) + (preco2 * quantidade2);
 
