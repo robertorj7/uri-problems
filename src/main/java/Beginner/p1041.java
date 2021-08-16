605b6526907f80b58fdd79271394e7808df4f44c
Roberto Jr <rrjr87@outlook.com>
2021-08-16 17:06:48 -0300

    added problem 1041

---
+++ src/main/java/Beginner/p1041.java
@@ -0,0 +1,34 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class p1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble(), y = sc.nextDouble();
+
+        if (x == 0 && y ==0) {
+            System.out.println("Origem");
+        }
+
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println("Q1");
+            } else if (y == 0){
+                System.out.println("Eixo X");
+            } else {
+                System.out.println("Q4");
+            }
+        } else if (x < 0) {
+            if (y > 0) {
+                System.out.println("Q2");
+            } else if (y == 0){
+                System.out.println("Eixo X");
+            } else {
+                System.out.println("Q3");
+            }
+        } else if (x == 0 && y != 0) {
+            System.out.println("Eixo Y");
+        }
+    }
+}
