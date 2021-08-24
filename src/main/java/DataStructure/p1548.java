772bb54e7021cf44eb462aeb38bf42e778323484
Roberto Jr <rrjr87@outlook.com>
2021-08-24 13:31:32 -0300

    finished problem 1548

--- src/main/java/DataStructure/p1548.java
+++ src/main/java/DataStructure/p1548.java
@@ -1,29 +1,47 @@
 package DataStructure;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class p1548 {
-    public class Banco {
-        private Integer numSenha;
-
-        public Banco(Integer numSenha) {
-            this.numSenha = numSenha;
-        }
-
-        public Integer getNumSenha() {
-            return numSenha;
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int casos = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int quantidade = scanner.nextInt();
+        int pessoasNaFila, senhaAtual, trocasDeLugar;
+        ArrayList<Integer> filaAtual = new ArrayList<>();
+        HashMap<Integer, Integer> numParaIndiceOriginal = new HashMap<>();
 
-        for (int i=0; i<casos; i++) {
-            int numClientes = sc.nextInt();
+        scanner.nextLine();
 
+        for(int i = 0; i < quantidade; i++) {
+            pessoasNaFila = scanner.nextInt();
+            trocasDeLugar = 0;
+
+            for(int j = 0; j < pessoasNaFila; j++) {
+                senhaAtual = scanner.nextInt();
+                filaAtual.add(senhaAtual);
+                numParaIndiceOriginal.put(senhaAtual, filaAtual.size() - 1);
+            }
+
+            numSenhaComparator comparator = new numSenhaComparator();
+            Collections.sort(filaAtual, comparator);
+
+            for(int j = 0; j < filaAtual.size(); j++) {
+                int indiceOriginal = numParaIndiceOriginal.get(filaAtual.get(j));
+
+                if(indiceOriginal != j) {
+                    trocasDeLugar++;
+                }
+            }
+
+            System.out.println(pessoasNaFila - trocasDeLugar);
+            filaAtual = new ArrayList<>();
         }
+
+        scanner.close();
     }
 
     public static class numSenhaComparator implements Comparator<Integer> {
