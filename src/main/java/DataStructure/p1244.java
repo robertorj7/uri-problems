8b2d3df8bf099e54e76209d640387f7c70361734
Roberto Jr <rrjr87@outlook.com>
2021-08-25 12:31:52 -0300

    added problem 1244

---
+++ src/main/java/DataStructure/p1244.java
@@ -0,0 +1,19 @@
+package DataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class p1244 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i=0; i<num; i++) {
+            List<String> lista = new ArrayList<>();
+            String linha = sc.nextLine();
+            System.out.println(linha);
+        }
+    }
+
+}
