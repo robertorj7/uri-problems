b758225b8b159c0014a8c1cad64c20ae23954428
Roberto Jr <rrjr87@outlook.com>
2021-08-25 10:35:25 -0300

    finished problem 1258

--- src/main/java/DataStructure/p1258.java
+++ src/main/java/DataStructure/p1258.java
@@ -2,28 +2,34 @@ package DataStructure;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class p1258 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int quantidade = sc.nextInt();
-        String nome, linhaUniforme, corUniforme, tamUniforme;
-        List<Uniforme> listaUniformes = new ArrayList<>();
+        sc.nextLine();
+        ArrayList<Uniforme> listaUniformes= new ArrayList<>(quantidade);
+        String name, uniformDetails, uniformColor, uniformSize;
 
-        for (int i=0; i<quantidade; i++) {
-            nome = sc.next();
-            linhaUniforme = sc.next();
-//            corUniforme = linhaUniforme.split(" ")[0];
-//            tamUniforme = linhaUniforme.split(" ")[1];
-
-//            listaUniformes.add(new Uniforme(nome, corUniforme, tamUniforme));
-            System.out.println(nome);
-            System.out.println(linhaUniforme);
+        for(int i = 0; i < quantidade; i++)
+        {
+            name = sc.nextLine();
+            uniformDetails = sc.nextLine();
+            uniformColor = uniformDetails.split(" ")[0];
+            uniformSize= uniformDetails.split(" ")[1];
+            listaUniformes.add(new Uniforme(name, uniformColor, uniformSize));
         }
 
-//        System.out.println(listaUniformes);
+        sc.close();
+        Collections.sort(listaUniformes, new UniformComparator());
+
+        for(Uniforme item : listaUniformes)
+        {
+            System.out.println(item.corUniforme + " " + item.tamUniforme + " " + item.nomeEstudante);
+        }
     }
 
     public static class Uniforme {
@@ -59,6 +65,28 @@ public class p1258 {
 
         public void setTamUniforme(String tamUniforme) {
             this.tamUniforme = tamUniforme;
+        }
+    }
+
+    public static class UniformComparator implements Comparator<Uniforme>
+    {
+        @Override
+        public int compare(Uniforme estudante0, Uniforme estudante1)
+        {
+            int res = estudante0.corUniforme.compareTo(estudante1.corUniforme);
+
+            if(res == 0)
+            {
+                res = estudante0.tamUniforme.compareTo(estudante1.tamUniforme);
+
+                if(res == 0)
+                {
+                    res = estudante0.nomeEstudante.compareTo(estudante1.nomeEstudante);
+                } else {
+                    res = -res;
+                }
+            }
+            return res;
         }
     }
 }
