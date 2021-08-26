fe789808870cf6b9a440ec5c0ad4f4b9914f56f4
Roberto Jr <rrjr87@outlook.com>
2021-08-26 16:26:01 -0300

    working on problem 2813

--- src/main/java/Beginner/p2813.java
+++ src/main/java/Beginner/p2813.java
@@ -17,16 +17,25 @@ public class p2813 {
             listaVolta.add(sc.next());
 
             if (i == 0) {
-                if (listaIda.get(i) == "chuva") {
+                if (listaIda.get(i).equals("chuva")) {
                     quantCasa++;
                 }
 
-                if (listaVolta.get(i) == "chuva") {
+                if (listaVolta.get(i).equals("chuva") && listaIda.get(i).equals("sol")) {
                     quantEscritorio++;
+                }
+            } else {
+                if (listaVolta.get(i-1).equals("sol")) {
+                    if (listaIda.get(i).equals("sol") && listaVolta.get(i).equals("chuva")) {
+                        quantEscritorio++;
+                    }
+
+                    if (listaIda.get(i).equals("chuva")) {
+                        quantCasa++;
+                    }
                 }
             }
         }
-
-
+        System.out.println(quantCasa + " " + quantEscritorio);
     }
 }
