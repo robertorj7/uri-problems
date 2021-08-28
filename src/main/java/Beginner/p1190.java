8281419a70b3535a0a52f5d79660ab392ca81154
Roberto Jr <rrjr87@outlook.com>
2021-08-28 16:22:44 -0300

    finished problem 1190

--- src/main/java/Beginner/p1190.java
+++ src/main/java/Beginner/p1190.java
@@ -9,22 +9,19 @@ public class p1190 {
         double soma = 0;
         char entrada = sc.next().toUpperCase().charAt(0);
         double[][] Matriz = new double[12][12];
-        for (int i=0; i<12; i++) {
-            for (int j=0; j<12; j++) {
+
+        for (int i=0; i<Matriz.length; i++) {
+            for (int j=0; j<Matriz.length; j++) {
                 Matriz[i][j] = sc.nextDouble();
             }
         }
 
-        for (int j=1; j<=5; j++) {
-            for (int i=11; i>=12-j; i--) {
-                soma += Matriz[i][j];
-            }
-        }
-
-        for (int j=10; j>=6; j--) {
-            for (int i=11; i>=j+1; i--) {
-                soma += Matriz[i][j];
-            }
+        for (int i=0; i<Matriz.length; i++) {
+        	for (int j=0; j<Matriz[i].length; j++) {
+        		if (j > i && j > Matriz.length-i-1) {
+        		    soma += Matriz[i][j];
+                }
+        	}
         }
 
         if (entrada == 'S') {
@@ -33,6 +30,5 @@ public class p1190 {
             System.out.println(String.format("%.1f", soma / 30));
         }
     }
-
 }
 
