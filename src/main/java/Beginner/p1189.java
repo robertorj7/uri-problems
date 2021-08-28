741d477f588b41e853bbb478ab7a885187cf6fc4
Roberto Jr <rrjr87@outlook.com>
2021-08-28 16:29:20 -0300

    added problem 1189

---
+++ src/main/java/Beginner/p1189.java
@@ -0,0 +1,33 @@
+package Beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class p1189 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double soma = 0;
+        char entrada = sc.next().toUpperCase().charAt(0);
+        double[][] Matriz = new double[12][12];
+
+        for (int i=0; i<Matriz.length; i++) {
+            for (int j=0; j<Matriz.length; j++) {
+                Matriz[i][j] = sc.nextDouble();
+            }
+        }
+
+        for (int i=0; i<Matriz.length; i++) {
+            for (int j=0; j<Matriz[i].length; j++) {
+                if ((j < i) && (j < Matriz.length-i-1)) {
+                    soma += Matriz[i][j];
+                }
+            }
+        }
+
+        if (entrada == 'S') {
+            System.out.println(String.format("%.1f", soma));
+        } else if (entrada == 'M') {
+            System.out.println(String.format("%.1f", soma / 30));
+        }
+    }
+}
