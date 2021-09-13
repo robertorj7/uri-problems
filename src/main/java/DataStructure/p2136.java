2c6c0e56079b4dec5c7ae511320d3798f5efa971
Roberto R Jr <rrjr87@outlook.com>
2021-09-13 12:32:03 -0300

    changing from Set to List because of ordering elements in problem 2163

--- src/main/java/DataStructure/p2136.java
+++ src/main/java/DataStructure/p2136.java
@@ -1,23 +1,26 @@
 package DataStructure;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class p2136 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Set<String> listaSim = new HashSet<>();
-        Set<String> listaNao = new HashSet<>();
-        String nome, decisao, ganhador, linha = sc.nextLine();
+        List<String> listaSim = new ArrayList<>();
+        List<String> listaNao = new ArrayList<>();
+        String nome, decisao, ganhador = "", linha = sc.nextLine();
 
         while (!linha.equals("FIM")) {
             nome = linha.split(" ")[0];
             decisao = linha.split(" ")[1];
 
             if (decisao.equals("SIM")) {
-                listaSim.add(nome);
+                if (!listaSim.contains(nome)) {
+                    listaSim.add(nome);
+                }
             } else if (decisao.equals("NAO")) {
                 listaNao.add(nome);
             }
@@ -25,9 +28,8 @@ public class p2136 {
             linha = sc.nextLine();
         }
 
-        for (String item : listaSim) {
-            System.out.println(item);
-        }
+        System.out.println(listaSim);
+        System.out.println(listaNao);
 
     }
 
