0cf487aa0294867fbb22c248f5b537473805085a
Roberto R Jr <rrjr87@outlook.com>
2021-09-14 12:03:56 -0300

    working on problem 2087

---
+++ src/main/java/Strings/p2087.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class p2087 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int entrada = sc.nextInt();
+        List<String> lista = new ArrayList<>();
+
+        while (entrada != 0) {
+            for (int i=0; i<entrada; i++) {
+                lista.add(sc.next());
+            }
+
+            lista = lista.stream().sorted().collect(Collectors.toList());
+            String saida = "Conjunto Bom";
+
+            for (int i = 0; i < entrada - 1; i++) {
+                if (lista.get(i + 1).startsWith(lista.get(i))) {
+                    saida = "Conjunto Ruim";
+                }
+            }
+
+            System.out.println(saida);
+
+            entrada = sc.nextInt();
+        }
+    }
+}
