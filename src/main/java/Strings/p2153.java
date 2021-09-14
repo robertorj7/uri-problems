aeaba9c5f2fa0b7411d3da0eb1a68b63a5a7ceaa
Roberto R Jr <rrjr87@outlook.com>
2021-09-14 11:06:42 -0300

    finished problem 2153

--- src/main/java/Strings/p2153.java
+++ src/main/java/Strings/p2153.java
@@ -5,12 +5,30 @@ import java.util.Scanner;
 public class p2153 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String entrada = sc.next();
-        while (sc.hasNext()) {
-            for (int i=0; i<entrada.length(); i++) {
-                System.out.println(entrada.charAt(i));
+
+        while(sc.hasNext()) {
+            String entrada = sc.nextLine();
+            Integer limite = (int) Math.floor(entrada.length() / 2);
+            Integer fim = entrada.length();
+            Integer aux = 0;
+
+            if(entrada.length() == 1) {
+                System.out.println(entrada);
+            } else if (limite >= 1) {
+                for (int i = limite; i >= 1; i--) {
+                    if(entrada.substring(fim-i, fim).equals(entrada.substring(fim-(2*i),fim-i))) {
+                        aux = i;
+                        System.out.println(entrada.substring(0, (fim - aux)));
+                    } else if (limite == 1) {
+                        System.out.println(entrada);
+                    }
+                }
+
+                if(limite > 1 && aux == 0) {
+                    System.out.println(entrada);
+                }
             }
-            entrada = sc.next();
         }
+        sc.close();
     }
 }
