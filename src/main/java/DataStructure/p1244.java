8b513e94112cb739496e36fe247fd9972b177152
Roberto R Jr <rrjr87@outlook.com>
2021-09-15 09:56:54 -0300

    working on problem 1244

--- src/main/java/DataStructure/p1244.java
+++ src/main/java/DataStructure/p1244.java
@@ -1,23 +1,17 @@
 package DataStructure;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class p1244 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = Integer.parseInt(sc.nextLine());
+        int num = sc.nextInt();
 
         for (int i=0; i<num; i++) {
-            List<String> lista = new ArrayList<>();
-            String palavras = sc.nextLine();
-            String[] arrayPalavras = palavras.split(" ");
+            Map<String, Integer> listaPalavras = new HashMap<>();
 
-            for (int j=0; j<arrayPalavras.length; j++) {
-                String palavra = palavras.split(" ")[j];
-
-            }
         }
     }
 
