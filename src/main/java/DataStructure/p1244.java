e027f7db2d70ade020c2416158d094b2b99a0998
Roberto R Jr <rrjr87@outlook.com>
2021-09-15 10:22:16 -0300

    finished problem 1244

--- src/main/java/DataStructure/p1244.java
+++ src/main/java/DataStructure/p1244.java
@@ -1,18 +1,30 @@
 package DataStructure;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class p1244 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int linhas = sc.nextInt();
+        sc.nextLine();
 
-        for (int i=0; i<num; i++) {
-            Map<String, Integer> listaPalavras = new HashMap<>();
+        List<String> listaPalavras = new ArrayList<>();
 
+        for (int i = 0; i < linhas; i++) {
+            String palavras = sc.nextLine();
+            listaPalavras.addAll(Arrays.stream(palavras.split(" ")).collect(Collectors.toList()));
+            String listaOrdenada = listaPalavras.stream()
+                .sorted(Comparator.comparingInt(String::length).reversed()
+                .thenComparing(String::compareTo)).collect(Collectors.joining(" "));
+
+            System.out.println(listaOrdenada);
+            listaPalavras.clear();
         }
+        sc.close();
     }
-
 }
