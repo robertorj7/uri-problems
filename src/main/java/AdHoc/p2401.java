6f34af043bc9ba1a4085c13c187b495c2ce7f9d4
Roberto R Jr <rrjr87@outlook.com>
2021-09-15 08:35:41 -0300

    working on problem 2401

--- src/main/java/AdHoc/p2401.java
+++ src/main/java/AdHoc/p2401.java
@@ -6,14 +6,20 @@ public class p2401 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt(), resultado = 1;
+        int num = sc.nextInt();
+        double resultado = 1.0;
 
         for (int i=0; i<num; i++) {
             int valor = Integer.parseInt(sc.next());
             char operacao = sc.next().charAt(0);
 
-            System.out.println(valor + " " + operacao);
+            if (operacao == '/') {
+                resultado /= valor;
+            } else if (operacao == '*') {
+                resultado *= valor;
+            }
         }
 
+        System.out.println(Math.round(resultado));
     }
 }
